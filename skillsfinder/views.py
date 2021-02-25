82563177a5476dc2d87438c4ff3c521ff201390e
Kaj Siebert <kaj@k-si.com>
2021-02-25 15:24:14 +0100

    View & Template sample implementations

--- skillsfinder/views.py
+++ skillsfinder/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from skillsfinder.models import Person
 
 
-# Create your views here.
 def index(request):
     context = {
         'site_name': 'Skillsfinder',
+        'people': Person.objects.all(),
     }
     return render(request, 'skillsfinder/index.html', context)
