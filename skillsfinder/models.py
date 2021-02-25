4eccaf8957a7828542b7fb27e2c995663a45bf65
Kaj Siebert <kaj@k-si.com>
2021-02-25 14:38:01 +0100

    Add sample implementation

--- skillsfinder/models.py
+++ skillsfinder/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
